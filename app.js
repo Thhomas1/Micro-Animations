16825e4fd1658fc2917b9b6aba9d7b52c88be493
Thhomas1 <thomasromero10@hotmail.com>
2023-01-24 16:46:21 -0300

    Last animations HOME

--- app.js
+++ app.js
@@ -7,5 +7,11 @@ const home = document.querySelector(".home");
 gsap.set('.feather', {scale: 0, transformOrigin: "center"});
 home.addEventListener('click', () => {
     gsap.fromTo(".home-svg", { scale: 1}, {scale: 0.9, yoyo: true, repeat:1 }); // seteado animacion explosion
-    gsap.fromTo(".feather", { y: -5, scale: 0}, { y: 20, scale: 1.5, duration: 1,}); // seteado hojita
+    gsap.fromTo(".feather", 
+    { y: -5, scale: 0}, 
+    { y: 20, scale: 1.5, duration: 1, stagger: 0.2}
+    ); // seteado hojita el stagger lo usamo para que aparezca varias veces y se sienta que son mas hojas
+
+    gsap.fromTo(".right-feather", { x:0 }, { x:5 });
+    
 });
